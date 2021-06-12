d19cc694dbd02199799190a74f1da35ecca09e9f
bexearn12 <earnshr@gmail.com>
2021-06-11 21:00:10 -0500

    commit tree 12ae8cb7e15bae85e9a5abbf942fe1bd9d437db0

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+//    scuberGreetingForFeet()
+// 1) gives customers a free sample if the ride is less than
+//    or equal to 400 feet
+// 2) charges 30 dollars for a distance over 2000 feet
+// 3) does not allow rides over 2500 feet
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+     return 'This one is on me!'
+  } else if (1999 < distance && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (distance)
+    return 'No can do.'
+  }
+
+function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  }
+//if tip is generous, reutrn 'Thank you so much.'
+//if top is not generous as generous, return 'Thank you.'
+//if anything else, return "Bye"
+//tip is a parameter
+
+function switchOnCharmFromTip(tip) {
+
+  switch(tip) {
+    case 'generous': 
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
+//switch case statement needs a default to end the code.
+//
+
